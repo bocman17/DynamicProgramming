4cf342cb9d022f2a3dd07c5519703d3c526fbcbf
Tomas Hejlik <tomas.hejlik@plus4u.net>
2023-03-19 11:41:43 +0100

    Add project files.

---
+++ DynamicProgramming/Fibonacci.cs
@@ -0,0 +1,20 @@
+﻿namespace DynamicProgramming
+{
+    public static class Fibonacci
+    {
+        public static long Fib(int n, Dictionary<long, long>? memo = null)
+        {
+            memo ??= new() { { 0, 0 }, { 1, 1 } };
+            if (memo.TryGetValue(n, out long value))
+            {
+                return value;
+            }
+            if(n <= 2)
+            {
+                return 1;
+            }
+            memo[n] = Fib(n - 1, memo) + Fib(n - 2, memo);
+            return memo[n];
+        }
+    }
+}
---
+++ OutputDP/Program.cs
@@ -0,0 +1,7 @@
+﻿using DynamicProgramming;
+using System.Diagnostics;
+
+Stopwatch stopwatch = Stopwatch.StartNew();
+Console.WriteLine(Fibonacci.Fib(50));
+stopwatch.Stop();
+Console.WriteLine("\nTime elapsed: {0}", stopwatch.ElapsedTicks);
